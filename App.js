1c1892268058529deca40db0083778234a0b67ec
yamil-90 <yamil.coloma90@gmail.com>
2021-12-31 10:05:32 -0300

    added new form functions

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Form from './src/components/Form';
 import Header from './src/components/Header';
 
 const App = () => {
+
+  const [fiat, setFiat] = useState('')
+    const [coin, setCoin] = useState('');
+    const [consultApi, setConsultApi] = useState(false)
+
+useEffect(() => {
+ if(consultApi){
+  console.log('cambio');
+ }
+}, [consultApi])
+
   return (
     <View>
       <Header/>
-      <Text>hola</Text>
+      <Image
+      style={styles.image}
+      source={require('./assets/img/cryptomonedas.png')}
+      />
+      <View style={styles.content}>
+      <Form
+      fiat={fiat}
+      setFiat={setFiat}
+      coin={coin}
+       setCoin={setCoin}
+       setConsultApi={setConsultApi}
+      />
+      </View>
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image:{
+    width:'100%',
+    height:150,
+    marginHorizontal: '2.5%'
+  },
+  content:{
+    marginHorizontal:'2.5%'
+  }
+})
---
+++ src/components/Form.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Pressable, Alert } from 'react-native'
+import { Picker } from '@react-native-picker/picker'
+import axios from 'axios'
+
+const Form = ({coin, setCoin, fiat, setFiat, setConsultApi}) => {
+    
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        const consultApi = async () => {
+            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD"
+            const result = await axios.get(url)
+            setCoins(result.data.Data);
+        }
+        consultApi()
+    }, [])
+
+    const getFiat = fiat => {
+        setFiat(fiat)
+    }
+
+    const getCoin = coin => {
+        setCoin(coin)
+    }
+
+    const quotePrice=(data)=>{
+        if(fiat.trim()==='' || coin.trim()===''){
+            showAlert()
+        }
+        setConsultApi(true)
+    }
+    const showAlert=()=>{
+Alert.alert(
+    'Error..',
+    'All the fields are mandatory',
+    [
+        {text:'ok'}
+    ]
+)
+    }
+
+    return (
+        <View>
+            <Text style={styles.label}>Fiat</Text>
+            <Picker
+                onValueChange={e => getFiat(e)}
+                selectedValue={fiat}
+                itemStyle={{ height: 120 }}
+            >
+                <Picker.Item label="-- Select --" value='' />
+                <Picker.Item label="USA Dollar" value='USD' />
+                <Picker.Item label="Euro" value='EUR' />
+                <Picker.Item label="Peso argentino" value='ARG' />
+            </Picker>
+            <Text style={styles.label}>Coin</Text>
+            <Picker
+                onValueChange={e => getCoin(e)}
+                selectedValue={coin}
+                itemStyle={{ height: 120 }}
+            >
+                <Picker.Item label="-- Select --" value='' />
+                {coins !== '' && coins.map(coinState => (
+                    <Picker.Item label={coinState.CoinInfo.FullName} value={coinState.CoinInfo.Name} key={coinState.CoinInfo.Id} />
+                ))}
+            </Picker>
+            <Pressable
+            style={styles.btn}
+            onPress={()=>quotePrice()}>
+                <Text style={styles.btnText}>Quote</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+export default Form
+
+const styles = StyleSheet.create({
+    label: {
+        fontFamily: 'Lato-Black',
+        textTransform: 'uppercase',
+        fontSize: 22,
+        marginVertical: 20
+    },
+    btn:{
+        backgroundColor:'#5e49e2',
+        padding:10,
+        marginTop:20
+    },
+    btnText:{
+        color:'#fff',
+        fontSize: 18,
+        textTransform:'uppercase',
+        textAlign:'center',
+        fontFamily: 'Lato-Black'
+    }
+})
